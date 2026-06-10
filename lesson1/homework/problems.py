f9a91c96a40066dc53b4901b22ad9954440deae0
EpicDJ8 <mlgonzalez.yanes@gmail.com>
2026-06-09 17:39:03 -0700

    Update problems.py

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+lucky_number = 7
+print(lucky_number)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+sleep = 7.5
+print(sleep)
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+fruit = apple
+print(fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
-
+city = Seattle
+country = USA
+print(country)
+print(city)
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+pet_name = mittens
+age = 5
+type = cat
+print(pet_name)
+print(age)
+print(type)
